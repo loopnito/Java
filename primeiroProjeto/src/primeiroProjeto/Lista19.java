0271062946aa852ef99e11b3535b35d540d55c27
loopnito <viniciusmmloop@gmail.com>
2025-10-03 16:49:01 -0300

    .

--- primeiroProjeto/src/primeiroProjeto/Lista19.java
+++ primeiroProjeto/src/primeiroProjeto/Lista19.java
@@ -7,11 +7,45 @@ public class Lista19 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-
+		
+		int ex1 = scanner.nextInt();
+		ex1(ex1);
+		int ex2 = scanner.nextInt();
+		ex2(ex2);
+		String ex3 = scanner.next();
+		ex3(ex3);
+		int ex4 = scanner.nextInt();
+		ex4(ex4);
+		
+		
 	}
 	
-	public static void ex1() {
-		
+	public static void ex1(int num) {
+		if (num < 0) {
+			System.out.println("menor d.e. ze,ro.");
+		} else if (num > 0) {
+			System.out.println("acima de zeroe.");
+		} else {
+			System.out.println("zereo!!!!");
+		}
+	}
+	
+	public static void ex2(int num) {
+		if (num > 18) {
+			System.out.println("Moar. Mao. Mai.r de. Id,ad,e.");
+		} else {
+			System.out.println("meo.nmenor. menor. de id.aed.");
+		}
+	}
+	
+	public static void ex3(String str) {
+		System.out.println("Ola, " + str);
+	}
+	
+	public static void ex4(int num) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num * i);
+		}
 	}
 
 }
